211312c99a08e940914c768dad864c86878d626f
Walter Morris <waltercmorris18@gmail.com>
2021-09-10 02:03:37 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names,eventName){
+    let customMessage = [];
+    for (let i = 0; i < names.length; i++){
+        customMessage.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return customMessage;
+};
+
+function countDown(beginNum){
+    let customNum = beginNum;
+    while (0 <= beginNum ){
+        console.log(beginNum --);
+    } 
+}
+
+countDown(10);
